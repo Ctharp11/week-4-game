376681bb47c50854ba79dc87d00c47e2acd5d8b7
Ctharp11 <ctharp1988@gmail.com>
2017-02-20 15:52:47 -0600

    updated

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,39 +1,39 @@
 
+$(document).ready(function() {
 
 var currentScore = 0;
 var wins = 0;
 var losses = 0; 
 var math = Math.floor(Math.random() * (102)) + 19;
 
-$(document).ready(function() {
-
-	var redCrystal = Math.floor(Math.random() * 12) + 1;
-	var blueCrystal = Math.floor(Math.random() * 12) + 1;
-	var yellowCrystal = Math.floor(Math.random() * 12) + 1;
-	var greenCrystal = Math.floor(Math.random() * 12) + 1;
+var redCrystal = Math.floor(Math.random() * 12) + 1;
+var blueCrystal = Math.floor(Math.random() * 12) + 1;
+var yellowCrystal = Math.floor(Math.random() * 12) + 1;
+var greenCrystal = Math.floor(Math.random() * 12) + 1;
 
 	$("#red").on("click", function () {
-		playGame();
 		currentScore += redCrystal; 
 		$(".score").html("<h3>" + currentScore + "</h3>");
+		playGame();
 	})
 	$("#blue").on("click", function () {
-		playGame();
 		currentScore += blueCrystal; 
 		$(".score").html("<h3>" + currentScore + "</h3>");	
+		playGame();
 	})
 	$("#yellow").on("click", function () {
-		playGame();
 		currentScore += yellowCrystal; 
 		$(".score").html("<h3>" + currentScore + "</h3>");	
+		playGame();
 	})
 	$("#green").on("click", function () {
-		playGame();
 		currentScore += greenCrystal; 
 		$(".score").html("<h3>" + currentScore + "</h3>");
+		playGame();
 	})
 
 function startGame () {
+
 
 	$(".winner").html("Wins: " + wins);
 	$(".loser").html("Losses: " + losses);
@@ -47,12 +47,10 @@ function playGame () {
 	if (currentScore === math) {
 		wins += 1;
 		$(".winner").text("Wins: " + wins);
-		 alert("You Win! \n The Random Number has been reset. Play again! \n Wins: " + wins);
 		 reset();
 	} else if (currentScore > math) {
 		losses += 1;
 		$(".loser").text("Losses: " + losses);
-		 alert("You lose. \n The Random Number has been reset. Play again! \n Losses: " + losses)
 		 reset();
 	}
 
@@ -64,13 +62,14 @@ function reset () {
 	blueCrystal = Math.floor(Math.random() * 12) + 1;
 	yellowCrystal = Math.floor(Math.random() * 12) + 1;
 	greenCrystal = Math.floor(Math.random() * 12) + 1;
-	currentScore = 0;
-	$(".score").html("0");
+	$(".score").empty();
 	$(".game-score").html("<h3> Random number: <br>	"+  + math + "</h3>");
+	currentScore = 0;
+
 
 }
 
 startGame();
-playGame();
+
 
 });
